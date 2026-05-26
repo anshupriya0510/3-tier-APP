baf02017eeef6c0c3cf642c74f2b22bfc732ead2
Anshu Priya <anshupriya05102004@gmail.com>
2026-05-26 07:37:41 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.cj6ia86mmyie.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root1234',
+    DB_DATABASE : 'webappdb'
+});
